7fc6f2a5436a39682c752db6afbca32d0642b3a3
Jessica Sanchez Padilla <jesssanc@c4r3s6.42urduliz.com>
2025-05-15 09:45:41 +0200

    resuelto cd

--- builtins/builtin_cd.c
+++ builtins/builtin_cd.c
@@ -6,12 +6,12 @@
 /*   By: jesssanc <jesssanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/12 13:05:58 by jesssanc          #+#    #+#             */
-/*   Updated: 2025/05/14 13:11:56 by jesssanc         ###   ########.fr       */
+/*   Updated: 2025/05/15 09:35:25 by jesssanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-/*
+
 static char	*get_key_value(char *key, t_shell *shell)
 {
 	int	i;
@@ -45,7 +45,6 @@ static void	update_env_variable(t_shell *shell, char *key, char *new_value)
 			new_entry = ft_strjoin(key, "=");
 			if (!new_entry)
 				return;
-			free(shell->envp[i]);
 			shell->envp[i] = ft_strjoin(new_entry, new_value);
 			free(new_entry);
 			return ;
@@ -54,7 +53,7 @@ static void	update_env_variable(t_shell *shell, char *key, char *new_value)
 	}
 }
 
-int	ft_cd(t_cmd *cmd, t_shell *shell)
+int	ft_cd(t_shell *shell, t_cmd *cmd)
 {
 	char	*path;
 	char	*oldpwd;
@@ -107,142 +106,4 @@ int	ft_cd(t_cmd *cmd, t_shell *shell)
 	update_env_variable(shell, "PWD", pwd);
 	free(pwd);
 	return (0);
-}
-*/
-
-int	ft_cd(t_shell *shell, t_cmd *cmd)
-{
-	char	*path;
-
-	path = NULL;
-	ft_change_oldpwd_env(shell);
-	if (cmd->argc > 1)
-		path = ft_strdup(cmd->argv[1]);
-	else
-	{
-		path = ft_get_env_value("HOME", shell);
-		if (!path)
-		{
-			cmd->error_message = ft_strdup("cd: HOME not set");
-			return (1);
-		}
-	}
-	if (chdir(path) != 0)
-	{
-		perror("cd");
-		free(path);
-		return (1);
-	}
-	ft_change_pwd_env(shell);
-	//free(path);
-	return (0);
-}
-
-void	ft_change_oldpwd_env(t_shell *shell)
-{
-	char	*oldpwd;
-	char	*new_oldpwd;
-	int		i;
-
-	oldpwd = NULL;
-	new_oldpwd = NULL;
-	i = 0;
-	oldpwd = ft_get_env_value("PWD", shell);
-	if (!oldpwd)
-		return;
-	new_oldpwd = ft_strjoin("OLDPWD=", oldpwd);
-	if (!new_oldpwd)
-	{
-		perror("malloc");
-		return;
-	}
-	while (shell->envp[i])
-	{
-		if (ft_strncmp(shell->envp[i], "OLDPWD=", 7) == 0)
-		{
-			free(shell->envp[i]);
-			shell->envp[i] = new_oldpwd;
-			return;
-		}
-		i++;
-	}
-	shell->envp = ft_add_to_env(shell->envp, new_oldpwd);
-	free(new_oldpwd);
-}
-
-void	ft_change_pwd_env(t_shell *shell)
-{
-	char	*cwd;
-	char	*new_pwd;
-	int		i;
-
-	new_pwd = NULL;
-	i = 0;
-	cwd = getcwd(NULL, 0);
-	if (!cwd)
-	{
-		perror("getcwd");
-		return;
-	}
-	new_pwd = ft_strjoin("PWD=", cwd);
-	if (!new_pwd)
-	{
-		perror("malloc");
-		return;
-	}
-	while (shell->envp[i])
-	{
-		if (ft_strncmp(shell->envp[i], "PWD=", 4) == 0)
-		{
-			free(shell->envp[i]);
-			shell->envp[i] = new_pwd;
-			return;
-		}
-		i++;
-	}
-	shell->envp = ft_add_to_env(shell->envp, new_pwd);
-	free(new_pwd);
-}
-char	*ft_get_env_value(const char *key, t_shell *shell)
-{
-	int		i;
-	int		len;
-
-	i = 0;
-	if (!key || !shell || !shell->envp)
-		return (NULL);
-	len = ft_strlen(key);
-	while (shell->envp[i])
-	{
-		if (ft_strncmp(shell->envp[i], key, len) == 0 &&
-			shell->envp[i][len] == '=')
-			return (shell->envp[i] + len + 1);
-		i++;
-	}
-	return (NULL);
-}
-char	**ft_add_to_env(char **envp, const char *new_var)
-{
-	int		i;
-	char	**new_envp;
-
-	i = 0;
-	while (envp[i])
-		i++;
-	new_envp = (char **)malloc((i + 2) * sizeof(char *));
-	if (!new_envp)
-	{
-		perror("malloc");
-		return (envp);
-	}
-	i = 0;
-	while (envp[i])
-	{
-		new_envp[i] = envp[i];
-		i++;
-	}
-	new_envp[i] = ft_strdup(new_var);
-	new_envp[i + 1] = NULL;
-	free(envp);
-	return (new_envp);
 }
